85c707ecdb0932fa4bd3a7a54b235914a0a4d98c
AlexeyGrebnev <90623727+AlexeyGrebnev@users.noreply.github.com>
2021-12-19 18:09:35 +0500

    Add files via upload

---
+++ Examples Massive 08.12.2021/Mas 2-Prog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+using namespace std;
+
+int main ()
+{
+    int a[25];
+    int count = 1;
+    int max = -1;
+    int r, range_min, range_max;
+    range_min = 2;
+    range_max = 280000
+}
