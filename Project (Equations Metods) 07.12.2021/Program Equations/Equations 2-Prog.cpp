5375a31d9bdd124ab40ff137865ae8bff54bc7d1
AlexeyGrebnev <90623727+AlexeyGrebnev@users.noreply.github.com>
2021-12-20 22:11:34 +0500

    Add files via upload

---
+++ Project (Equations Metods) 07.12.2021/Program Equations/Equations 2-Prog.cpp
@@ -0,0 +1,30 @@
+// МЕТОД НЬЮТОНА
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+    // f(x) = x^3 - x - 1
+    // f'(x) = 3x^2 - 1
+    // [1 , 2]
+    // f(a) * f ''(a) >> 0
+    // (1 - 1 - 1) * (1*6) - проверка 
+    // f(b) * f''(b) > 0 
+    // (8 - 2 - 1) * (6*2) > 0 => x0 = b
+
+int main ()
+{
+    setlocale(LC_ALL, "RUS");
+    float x, xPred; // Икс текущий и предыдущий 
+    // f(b) * f''(b) > 0 
+    // (8 - 2 - 1) * (6*2) > 0 => x0 = b
+    x = 2; // берём значение x0, которое удовлетворяет одному из двух усл (см выше)
+    xPred = 0; // делаем заведомо сильно меньше x, чтобы цикл правильно сработал
+    double eps = 0.000001;
+     while (abs(x - xPred) > eps)
+     {
+         xPred = x;
+         x = xPred -(pow(xPred, 3) - xPred - 1) / (3*pow(xPred, 2) - 1);
+     }
+     cout << "Корень по Методу Ньютона =>  " << x << endl;
+     return 0;
+}
